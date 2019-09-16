9a01348c04429f54207e15231687991ec3d8e93d
Ahsan-Rahim <Ahsan.Rahim@ibm.com>
2019-09-16 10:54:05 -0500

    added sorting with stuff

--- src/routers/task.js
+++ src/routers/task.js
@@ -5,12 +5,17 @@ const router = new express.Router()
 
 
 //GET /tasks?completed
-//GET/tasks?limit||skip 
+//GET/tasks?limit||skip
+// GET /tasks?sortBy= createdAt_asc
 router.get('/tasks',auth,async(req,res) => {
     const match = {}
-    
+    const sort = {}
     if(req.query.completed){
         match.isCompleted = req.query.completed === 'true'
+    }
+    if(req.query.sortBy){
+        const parts = req.query.sortBy.split('_')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
     }
     try{
         await   req.user.populate({
@@ -18,7 +23,8 @@ router.get('/tasks',auth,async(req,res) => {
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         }).execPopulate()
         res.send(req.user.tasks)
